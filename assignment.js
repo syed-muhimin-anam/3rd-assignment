9796328cef040c24539cfa166692c83dcca03256
Syed Muhimin Anam <syed.muhimin.anam@.com>
2021-01-22 18:34:57 -0800

    done

--- assignment.js
+++ assignment.js
@@ -1,32 +1,42 @@
 //
 
 function kilometerToMeter(kilometer){
-
+    if (typeof(kilometer) != "number" || kilometer < 0){
+        return 'plese insert valid number for kilometer';
+    }
     var meter = kilometer * 1000;
     return meter;
 }
-
 var result = kilometerToMeter(27);
-// console.log(result);
+console.log(result);
  
 
-//second problem
-function budgetCalculator(ghori, phone ,laptop){
+//second problem:
 
+function budgetCalculator(ghori, phone ,laptop){
+    if (typeof(ghori) != "number" || typeof(phone) != "number" || typeof(laptop) != "number") {
+        return 'please check your quantity';
+    }
+    else if (ghori < 0 || phone < 0 || laptop < 0) {
+        return 'please check your quantity';
+    }
     var ghoriCost = ghori * 50;
     var phoneCost = phone * 100;
     var laptopCost = laptop * 500;
-    var totalCost = ghoriCost + phoneCost + laptopCost;
+    var totalCost = ghoriCost + phoneCost + laptopCost; /**total budget */
     return totalCost;
 }
-
 var totalBudget = budgetCalculator(10,7,5);
-// console.log(totalBudget);
+console.log(totalBudget);
 
-//third problem
+
+//third problem:
 
 function hotelCost(days){
     var cost = 0;
+    if (typeof(days) != "number" || days < 0) {
+        return "plese check your days";
+    }
     if(days <= 10){
         cost = days * 100;
     }
@@ -41,27 +51,30 @@ function hotelCost(days){
         var secondTenDay = 10 * 80;
         var extraDay = days - 20;
         var thirdPartDay = extraDay * 50;
-        cost = firstTenDay + secondTenDay + thirdPartDay;
+        cost = firstTenDay + secondTenDay + thirdPartDay;/**all day cost */
     }
     return cost;
 }
 
 var totalMoneySpend = hotelCost(37);
-// console.log(totalMoneySpend);
+console.log(totalMoneySpend);
 
 
 
-//fourth problem
+//fourth problem:
 
 function megaFriend(name){
-    var large = name[0];
+    var largeName = name[0];
     for (var i = 0; i < name.length; i++){
+        if(typeof(name[i]) == 'number' || typeof(name[i]) == 'boolean'){
+            return 'sorry you dont wright your friend name';
+        }
         var check = name[i];
-        if(check.length > large.length){
-            large = check;
+        if(check.length > largeName.length){
+            largeName = check;
         }
     }
-    return large;
+    return largeName;
 }
 var largestName = megaFriend( ['anam','urmi','mashuk','nur','himu','tuba']);
-console.log(largestName)
+ console.log(largestName);
